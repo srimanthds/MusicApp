f23191179e1a87242a7cd8772e5aeae08cc9e47b
srimanthds <srimanthdudi95@gmail.com>
2017-02-21 19:29:31 +0530

    Initial commit

---
+++ app/src/main/java/com/acadgild/musicapp/activities/ActivityCurrentSong.java
@@ -0,0 +1,22 @@
+package com.acadgild.musicapp.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.acadgild.musicapp.R;
+
+/**
+ * Created by AcadGildMentor on 6/9/2015.
+ */
+public class ActivityCurrentSong extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.act_current_song);
+
+
+    }
+}
